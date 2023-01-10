0a270e3423c272eb54ff3745fbbe0e4742d961a6
Eris Susanto <erissusanto997@gmail.com>
2023-01-10 17:58:39 +0700

    Add registration feature

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,47 @@
 const { User, MyBook } = require('../models')
+const Mailjet = require('node-mailjet')
+const mailjet = new Mailjet.apiConnect(process.env.API_KEY_MAILJET, process.env.SECRET_KEY_MAILJET)
 
 class Controller{
-    
+    static async register(req, res, next){
+        try {
+            const { username, email, password } = req.body
+            const newUser = await User.create({ username, email, password })
+
+            const request = await mailjet
+            .post("send", {'version': 'v3.1'})
+            .request({
+            "Messages":[
+                {
+                "From": {
+                    "Email": "erissusanto997@gmail.com",
+                    "Name": "Eris"
+                },
+                "To": [
+                    {
+                    "Email": email,
+                    "Name": username
+                    }
+                ],
+                "Subject": "Hai, good readers.",
+                "TextPart": "My first Mailjet email",
+                "HTMLPart": `<h3>Dear ${username}, welcome to GarageReading</a>!</h3><br />Hope you get new insight`,
+                "CustomID": `user${newUser.id}`
+                }
+            ]
+            })
+           
+            res.status(201).json({
+                id: newUser.id,
+                username:
+                newUser.username,
+                email: newUser.email,
+                notification: request.response.data.Messages[0]
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const Controller = require('../controllers')
 
-
+router.post("/register", Controller.register)
 
 module.exports = router
