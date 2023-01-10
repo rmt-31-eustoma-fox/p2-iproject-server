2fe929f9dce7cf170580b7a5b304e1910ecedd5f
Eris Susanto <erissusanto997@gmail.com>
2023-01-10 18:51:29 +0700

    add google-sign-in feature

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const Controller = require('../controllers')
 
 router.post("/register", Controller.register)
 router.post("/login", Controller.login)
+router.post("/google-sign-in", Controller.loginByGoogle)
 
 module.exports = router
