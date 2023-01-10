1d69a7ef1a088d8b168d04826fbc7ea574009fe7
Aldrino Wibowo <mraldrino@gmail.com>
2023-01-10 11:35:01 +0700

    get product and category done

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ router.post("/register", Controller.register);
 router.post("/login", Controller.login);
 router.post("/google-sign-in", Controller.googleSignin);
 
+router.get("/products", Controller.products);
+router.get("/categories", Controller.categories);
+
 router.use(authentication);
 
 module.exports = router;
