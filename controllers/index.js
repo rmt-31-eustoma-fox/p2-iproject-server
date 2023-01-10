130c0732a32638d426c1d35239db2512d646c350
Aldrino Wibowo <mraldrino@gmail.com>
2023-01-10 11:02:49 +0700

    login feature done

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 const { User, Cart, OrderHistory, Category, Product } = require("../models");
+const { hashPassword, comparePassword } = require("../helpers/bcrypt");
+const { generateToken } = require("../helpers/jwt");
 
 class Controller {
   static async register(req, res, next) {
@@ -8,6 +10,29 @@ class Controller {
       const newUser = await User.create({ email, password });
 
       res.status(201).json({ message: `user with email ${newUser.email} has been created` });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user) throw { name: "invalidLogin" };
+
+      const validPassword = comparePassword(password, user.password);
+      if (!validPassword) throw { name: "invalidLogin" };
+
+      const payload = {
+        id: user.id,
+      };
+      const access_token = generateToken(payload);
+
+      res.status(200).json({ access_token, username: user.username });
     } catch (error) {
       next(error);
     }
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,6 +2,17 @@ const errorHandler = (err, req, res, next) => {
   let code = 500;
   let message = "Internal Server Error";
 
+  if (err.name === "SequelizeValidationError" || err.name === "SequelizeUniqueConstraintError") {
+    code = 400;
+    message = err.errors[0].message;
+  } else if (err.name === "invalidLogin") {
+    code = 401;
+    message = "Wrong Email or Password";
+  } else if (err.name === "invalid_token" || err.name === "JsonWebTokenError") {
+    code = 401;
+    message = "Invalid Token";
+  }
+
   res.status(code).json({ message });
 };
 
