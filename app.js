c38b6de4a5b1c581d645294f4861f0c5091d9631
whooptywoo <albertusrhz@gmail.com>
2023-01-12 14:18:16 +0700

    Rename API Docs File

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 const routes = require('./routes');
 const cors = require('cors');
+const { config } = require("dotenv");
+
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+
 
 app.use(cors());
 app.set('view engine', 'ejs');
